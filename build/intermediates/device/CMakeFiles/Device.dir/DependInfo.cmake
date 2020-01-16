d8ba7630e62267c15cd8744932e46c5d15db9a72
xyttaotao <15150667798@163.com>
2020-01-16 00:35:05 -0800

    add license

---
+++ build/intermediates/device/CMakeFiles/Device.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ascend/gitxyt/sample-crowdcounting/src/crowd_counting/crowd_counting.cpp" "/home/ascend/gitxyt/sample-crowdcounting/build/intermediates/device/CMakeFiles/Device.dir/crowd_counting/crowd_counting.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ascend/gitxyt/sample-crowdcounting/src/."
+  "/home/ascend/.mindstudio/huawei/ddk/1.31.T15.B150/ddk/include/inc"
+  "/home/ascend/.mindstudio/huawei/ddk/1.31.T15.B150/ddk/include/third_party/protobuf/include"
+  "/home/ascend/.mindstudio/huawei/ddk/1.31.T15.B150/ddk/include/third_party/cereal/include"
+  "/home/ascend/.mindstudio/huawei/ddk/1.31.T15.B150/ddk/include/libc_sec/include"
+  "/home/ascend/ascend_ddk/include"
+  "/home/ascend/ascend_ddk/include/third_party/ffmpeg"
+  "/home/ascend/.mindstudio/huawei/ddk/1.31.T15.B150/ddk/include/third_party/opencv/include"
+  "/home/ascend/gitxyt/sample-crowdcounting/src/common"
+  "/home/ascend/gitxyt/sample-crowdcounting/src/video_decode"
+  "/home/ascend/gitxyt/sample-crowdcounting/src/crowd_counting"
+  "/home/ascend/gitxyt/sample-crowdcounting/src/crowd_counting_post_process"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/intermediates/device/CMakeFiles/Device.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Device.dir/crowd_counting/crowd_counting.cpp.o"
+  "/home/ascend/gitxyt/sample-crowdcounting/build/outputs/libDevice.pdb"
+  "/home/ascend/gitxyt/sample-crowdcounting/build/outputs/libDevice.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Device.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
